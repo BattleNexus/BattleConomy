315c350bb7286b021648cc2c87fe7ff4f93b621b
Adam Lavin <adam@lavoaster.co.uk>
2012-12-10 02:43:02 +0000

    Static commands!
    Just committing what I currently have!

--- src/net/battlenexus/bukkit/economy/BattleCommands.java
+++ src/net/battlenexus/bukkit/economy/BattleCommands.java
@@ -7,19 +7,45 @@ import org.bukkit.entity.Player;
 
 public class BattleCommands implements CommandExecutor  {
 	
-	Player player;
 	
-	public BattleCommands(BattleConomy battleconomy) {
+	public BattleCommands(BattleConomy battleconomy)
+	{
 		//I'm sure this will come in use at sometime.
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
-		player = (Player) sender;
+		//I are too lazy to write the dynamic commands system
+		if(args.length < 1){
+			String[] newargs = {"help"};
+			if(sender instanceof Player)
+			{
+				newargs[0] = "balance"; 
+			}
+			return onCommand(sender, cmd, label, newargs);
+		}
 		
-		//TODO Add commands
+		sender.sendMessage("--------- BEGIN DEBUG INFO ---------");
+		sender.sendMessage("Label Used");
+		sender.sendMessage(label);
+		sender.sendMessage("Arguments:");
+		sender.sendMessage(args);
+		sender.sendMessage("---------  END DEBUG INFO  ---------");
+		sender.sendMessage("");
 		
-		return false;
+		String messageToSend = "Invalid Arguement";
+		
+		if(args[0].equalsIgnoreCase("balance")){
+			messageToSend = "You have £20.34";
+		}
+		
+		if(args[0].equalsIgnoreCase("help")){
+			messageToSend = "Help? What Help? Your on your own for this one.";
+		}
+		
+		sender.sendMessage(messageToSend);
+		
+		return true;
 	}
 }
