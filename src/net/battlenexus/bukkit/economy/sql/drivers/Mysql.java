ec7a5dad7aa001940391f03c53be779c235a817f
Adam Lavin <adam@lavoaster.co.uk>
2013-01-03 21:30:47 +0000

    Forgot to remove C3P0 Import

--- src/net/battlenexus/bukkit/economy/sql/drivers/Mysql.java
+++ src/net/battlenexus/bukkit/economy/sql/drivers/Mysql.java
@@ -7,8 +7,6 @@ import java.sql.SQLException;
 
 import net.battlenexus.bukkit.economy.sql.SqlClass;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
-
 public class Mysql extends SqlClass {
 
     @Override
