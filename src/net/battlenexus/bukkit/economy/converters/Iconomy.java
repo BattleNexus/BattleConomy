0f80a29ce7726d1b91da487d5202a62010b04979
Adam Lavin <adam@lavoaster.co.uk>
2013-01-27 03:01:08 +0000

    >---- OPTIMIZE THIS LATER ----<

--- src/net/battlenexus/bukkit/economy/converters/Iconomy.java
+++ src/net/battlenexus/bukkit/economy/converters/Iconomy.java
@@ -25,7 +25,7 @@ public class Iconomy extends ConverterClass {
             while((line = file.readLine()) != null) {
                 String[] miniDb = line.split(" ");
                 
-                Api.sql.build("INSERT IGNORE INTO " + Api.sql.prefix
+                Api.sql.build("INSERT IGNORE INTO " + Api.sql.prefix + "players SET username="+miniDb[0]+";"+"INSERT IGNORE INTO " + Api.sql.prefix
                         + "balances SELECT "+miniDb[0]+",id,"+Double.parseDouble(miniDb[1].replace("balance:", ""))+" FROM " + Api.sql.prefix
                         + "players WHERE username="+miniDb[0]+";");
                 i++;
